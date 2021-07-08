b3051cabce94e4bdde412c0629132964f08afd08
manil-123 <punmanil3@gmail.com>
2021-07-08 19:11:07 +0545

    final

--- lib/app_navigator.dart
+++ lib/app_navigator.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pokedex/pokedex_view.dart';
+import 'package:pokedex/pokemon_details_view.dart';
 import 'bloc/nav_cubit.dart';
 
 class AppNavigator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<NavCubit, int>(builder: (context, pokemonId) {
+    return BlocBuilder<NavCubit, int?>(builder: (context, pokemonId) {
       return Navigator(
         pages: [
           MaterialPage(child: PokedexView()),
